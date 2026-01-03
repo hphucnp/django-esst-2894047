0f958b404bd6efc99562ffca7ca682a3d024c45b
Phuc Nguyen <hphuc.np@gmail.com>
2026-01-03 09:46:08 +0000

    wip

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('popular-notes', views.PopularNotesListView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailView.as_view())
+    path('notes/<int:pk>', views.NoteDetailView.as_view(), name="note.detail")
 ]
