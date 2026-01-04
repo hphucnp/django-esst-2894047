7e351ec6d6721d9a129cbe00aa7e3fa13db1e296
phuc.nguyen <hphuc.np@gmail.com>
2026-01-04 12:02:11 +0700

    wip

--- home/views.py
+++ home/views.py
@@ -1,19 +1,26 @@
 from datetime import datetime
+
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'home/welcome.html'
-    extra_context = {'today': datetime.today()}
+    template_name = "home/welcome.html"
+    extra_context = {"today": datetime.today()}
 
-class AuthorizedView(LoginRequiredMixin, TemplateView):
-    template_name = 'home/authorized.html'
-    login_url = '/admin'
-    
+
 class LoginInterfaceView(LoginView):
-    template_name = 'home/login.html'
-    
+    template_name = "home/login.html"
+
+
 class LogoutInterfaceView(LogoutView):
-    template_name = 'home/logout.html'
+    template_name = "home/logout.html"
+    next_page = "/"
+    http_method_names = ['get', 'post', 'options']
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect(self.next_page)
