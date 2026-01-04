b613d01a3e1bfc7f7b82f01a03fd5de7a28fc1cf
phuc.nguyen <hphuc.np@gmail.com>
2026-01-04 16:15:18 +0700

    wip

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 # Create your views here.
@@ -16,10 +18,16 @@ class LoginInterfaceView(LoginView):
     template_name = "home/login.html"
 
 
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = "home/signup.html"
+    success_url = reverse_lazy("login")
+
+
 class LogoutInterfaceView(LogoutView):
     template_name = "home/logout.html"
     next_page = "/"
-    http_method_names = ['get', 'post', 'options']
+    http_method_names = ["get", "post", "options"]
 
     def get(self, request, *args, **kwargs):
         logout(request)
